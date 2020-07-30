bfc7a225a7c1a833c4f92411150531854acbf213
sepid <shah0226@algonquinlive.com>
2020-07-29 22:30:50 -0400

    working on project

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String ACTIVITY_NAME = "MAIN_ACTIVITY";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
