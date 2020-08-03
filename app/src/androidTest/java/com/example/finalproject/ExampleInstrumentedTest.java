a4d71201bd0cf354ba7867e440fe1ddb274b3429
NingxinZhao <zhao0191@algonquinlive.com>
2020-08-03 11:25:34 -0400

    page 2 result page

--- app/src/androidTest/java/com/example/finalproject/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/finalproject/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.finalproject", appContext.getPackageName());
+        assertEquals("com.example.FinalProject", appContext.getPackageName());
     }
 }
