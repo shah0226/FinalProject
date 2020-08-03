c70cab24cfdaba3c5af904afe96ab6c5cf587827
NingxinZhao <zhao0191@algonquinlive.com>
2020-08-03 11:30:21 -0400

    page 2 result page

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView artistname = findViewById(R.id.artistname);
-        Button resultButton = findViewById(R.id.resultbutton);
-        resultButton.setOnClickListener(click -> {
-            Intent goToResult = new Intent(MainActivity.this, result_page.class);
-            goToResult.putExtra("artistname", artistname.getText().toString());
-            startActivity(goToResult);
-        });
+        Button logButton = findViewById(R.id.searchbutton);
+        logButton.setOnClickListener(click -> startactivitymethod());
 
     }
+
+    private void startactivitymethod() {
+        Intent resultPage = new Intent(this, MainActivity.class);
+        startActivity(resultPage);
+    }
+
 }
